4c94415b59f99bf088295079ed57d5ad2187bbc5
AMRESH-SLACK <67509274+AMRESH-SLACK@users.noreply.github.com>
2020-08-07 21:05:04 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const $imageButton = document.getElementById("image");
+$imageButton.onclick = () =>{
+    location.href = "./image.html";
+};
+const $image2Button = document.getElementById("image2");
+$image2Button.onclick = () =>{
+    location.href = "./image2.html";
+};
+const $image3Button = document.getElementById("image3");
+$image3Button.onclick = () =>{
+    location.href = "./image3.html";
+};
+const $image4Button = document.getElementById("image4");
+$image4Button.onclick = () => {
+    location.href = "./image4.html";
+};
+const $heliButton = document.getElementById("heli");
+$heliButton.onclick = ()=>{
+    location.href = "./heli.html";
+};
+const $heli2Button = document.getElementById("heli2");
+$heli2Button.onclick = () =>{
+    location.href = "./heli2.html";
+};
+const $heli3Button = document.getElementById("heli3");
+$heli3Button.onclick = () =>{
+    location.href = "./heli3.html";
+};
+const $heli4Button = document.getElementById("heli4");
+$heli4Button.onclick = () =>{
+    location.href = "./heli4.html";
+};
+const $trainerButton = document.getElementById("trainer");
+$trainerButton.onclick = () =>{
+    location.href = "./trainer.html";
+};
+const $trainer2Button = document.getElementById("trainer2");
+$trainer2Button.onclick = () =>{
+    location.href = "./trainer2.html";
+};
+const $trainer3Button = document.getElementById("trainer3");
+$trainer3Button.onclick = () =>{
+    location.href = "./trainer3.html";
+};
+const $transButton = document.getElementById("trans");
+$transButton.onclick = () =>{
+    location.href = "./trans.html";
+};
+const $trans2Button = document.getElementById("trans2");
+$trans2Button.onclick = ()=>{
+    location.href = "./trans2.html";
+};
+const $trans3Button = document.getElementById("trans3");
+$trans3Button.onclick = () =>{
+    location.href = "./trans3.html";
+};
+const $trans4Button = document.getElementById("trans4");
+$trans4Button.onclick = () =>{
+    location.href = "./trans4.html";
+};
